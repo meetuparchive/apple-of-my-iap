75390ff7c2d154ab04abe3e699f2171126a3acb4
Jose Rodriguez <jose@meetup.com>
2015-03-27 16:00:13 -0400

    Receipt renderer.

---
+++ src/main/scala/com/meetup/iap/ReceiptRenderer.scala
@@ -0,0 +1,47 @@
+package com.meetup.iap
+
+import com.meetup.base.util.TimeUtil
+import com.meetup.business.iap.AppleApi.{ReceiptResponse, ReceiptInfo}
+
+import java.util.Date
+
+import org.json4s.JsonDSL._
+import org.json4s.native.JsonMethods._
+import org.json4s.JValue
+
+object ReceiptRenderer {
+  private def asGmt(date: Date): Date = TimeUtil.translateTime(date, TimeUtil.MEETUP_TZ, TimeUtil.GMT_TZ)
+
+  def apply(response: ReceiptResponse): String = {
+    pretty(render(
+      ("receipt" -> renderReceipt(response.receipt)) ~
+      ("latest_receipt_info" -> response.latestReceiptInfo.map(renderReceipt)) ~
+      ("latest_expired_receipt_info" -> response.latestExpiredReceiptInfo.map(renderReceipt)) ))
+  }
+
+  private def renderReceipt(receiptInfo: ReceiptInfo): JValue = {
+
+    val gmtOrigPurchaseDate = asGmt(receiptInfo.originalPurchaseDate)
+    val origPurchaseDate = TimeUtil.getFormattedDate(gmtOrigPurchaseDate, TimeUtil.timedateFormat)
+    val origPurchaseDateMs = gmtOrigPurchaseDate.getTime
+
+    val gmtPurchaseDate = asGmt(receiptInfo.purchaseDate)
+    val purchaseDate = TimeUtil.getFormattedDate(gmtPurchaseDate, TimeUtil.timedateFormat)
+    val purchaseDateMs = gmtPurchaseDate.getTime
+
+    parse(s"""{
+      "item_id": "521129812",
+      "bid": "com.meetup.Meetup",
+      "product_id": "${receiptInfo.productId}",
+      "transaction_id": "${receiptInfo.transactionId}",
+      "purchase_date": "$purchaseDate Etc/GMT",
+      "purchase_date_ms": "$purchaseDateMs"
+      "original_transaction_id": "${receiptInfo.originalTransactionId}",
+      "original_purchase_date": "$origPurchaseDate Etc/GMT",
+      "original_purchase_date_ms": "$origPurchaseDateMs",
+      "quantity": "1",
+      "bvrs": "20120427",
+    }""")
+  }
+
+}
---
+++ src/test/scala/com/meetup/iap/ReceiptRendererTest.scala
@@ -0,0 +1,62 @@
+package com.meetup.iap
+
+import com.meetup.scalacheck.ScalaTestPropertySpec
+import com.meetup.business.iap.AppleApi
+import com.meetup.business.iap.AppleApi.{ReceiptResponse, ReceiptInfo}
+import org.joda.time.DateTime
+
+class ReceiptRendererTest extends ScalaTestPropertySpec {
+
+  property("Renderer should produce valid dates") {
+    val purchaseDate = new DateTime().withMillis(0).toDate
+    val originalPurchaseDate = purchaseDate
+
+    val receipt = ReceiptInfo(
+      productId = "product_123",
+      transactionId = "trans_1234",
+      purchaseDate = purchaseDate,
+      originalPurchaseDate = originalPurchaseDate,
+      originalTransactionId = "orig_trans_1233"
+    )
+
+    val json = ReceiptRenderer(ReceiptResponse(receipt))
+    val response = AppleApi.parseResponse(json)
+
+    response.receipt.purchaseDate should equal (purchaseDate)
+    response.receipt.originalPurchaseDate should equal (originalPurchaseDate)
+  }
+
+  property("Renderer should produce valid latest receipts.") {
+    val purchaseDate = new DateTime().withMillis(0).minusMonths(1).toDate
+    val originalPurchaseDate = purchaseDate
+
+    val latestPurchaseDate = new DateTime().withMillis(0).toDate
+
+    val receipt = ReceiptInfo(
+      productId = "product_123",
+      transactionId = "trans_1234",
+      purchaseDate = purchaseDate,
+      originalPurchaseDate = originalPurchaseDate,
+      originalTransactionId = "orig_trans_1233"
+    )
+
+    val latestReceipt = ReceiptInfo(
+      productId = "product_123",
+      transactionId = "trans_1234",
+      purchaseDate = latestPurchaseDate,
+      originalPurchaseDate = originalPurchaseDate,
+      originalTransactionId = "orig_trans_1233"
+    )
+
+    val json = ReceiptRenderer(ReceiptResponse(receipt, latestReceiptInfo = Some(latestReceipt)))
+    val response = AppleApi.parseResponse(json)
+
+    response.receipt.purchaseDate should equal (purchaseDate)
+    response.receipt.originalPurchaseDate should equal (originalPurchaseDate)
+
+    response.latestReceiptInfo.nonEmpty should be (true)
+    response.latestReceiptInfo.map { latest =>
+      latest.purchaseDate should equal(latestPurchaseDate)
+    }
+  }
+}
