6293c770d81bc3826da6d8e9c928e4d333804575
Daron <daron@meetup.com>
2015-04-22 17:54:00 -0400

    return latestReceipt in verifyReceipt call

--- src/main/scala/com/meetup/iap/receipt/ReceiptRenderer.scala
+++ src/main/scala/com/meetup/iap/receipt/ReceiptRenderer.scala
@@ -18,7 +18,8 @@ object ReceiptRenderer {
       ("receipt" -> renderReceipt(response.receipt)) ~
         ("latest_receipt_info" -> response.latestReceiptInfo.map(renderReceipt)) ~
         ("latest_expired_receipt_info" -> response.latestExpiredReceiptInfo.map(renderReceipt)) ~
-        ("status" -> response.statusCode)))
+        ("status" -> response.statusCode)~
+        ("latestReceipt" -> response.latestReceipt)))
   }
 
   private def renderReceipt(receiptInfo: ReceiptInfo): JValue = {
