b9ba8feb1be3ec8025ce9d2934f998fe3f45aba7
Jose Rodriguez <jose@meetup.com>
2015-05-05 16:21:06 -0400

    Added test and made others compilable.

--- src/test/scala/com/meetup/iap/receipt/ReceiptRendererTest.scala
+++ src/test/scala/com/meetup/iap/receipt/ReceiptRendererTest.scala
@@ -11,52 +11,19 @@ class ReceiptRendererTest extends ScalaTestPropertySpec {
     val purchaseDate = new DateTime().withMillis(0).toDate
     val originalPurchaseDate = purchaseDate
 
-    val receipt = ReceiptInfo(
-        originalPurchaseDate,
-        "orig_trans_1233",
-        "trans_1234",
-        purchaseDate,
-        "product_123"
-    )
-
-    val json = ReceiptRenderer(ReceiptResponse(receipt))
-    val response = AppleApi.parseResponse(json)
-
-    response.receipt.purchaseDate should equal (purchaseDate)
-    response.receipt.originalPurchaseDate should equal (originalPurchaseDate)
-  }
-
-  property("Renderer should produce valid latest receipts.") {
-    val purchaseDate = new DateTime().withMillis(0).minusMonths(1).toDate
-    val originalPurchaseDate = purchaseDate
-
-    val latestPurchaseDate = new DateTime().withMillis(0).toDate
-
-    val receipt = ReceiptInfo(
-      productId = "product_123",
-      transactionId = "trans_1234",
-      purchaseDate = purchaseDate,
-      originalPurchaseDate = originalPurchaseDate,
-      originalTransactionId = "orig_trans_1233"
-    )
-
-    val latestReceipt = ReceiptInfo(
-      productId = "product_123",
-      transactionId = "trans_1234",
-      purchaseDate = latestPurchaseDate,
-      originalPurchaseDate = originalPurchaseDate,
-      originalTransactionId = "orig_trans_1233"
-    )
-
-    val json = ReceiptRenderer(ReceiptResponse(receipt, latestReceiptInfo = Some(latestReceipt)))
-    val response = AppleApi.parseResponse(json)
-
-    response.receipt.purchaseDate should equal (purchaseDate)
-    response.receipt.originalPurchaseDate should equal (originalPurchaseDate)
-
-    response.latestReceiptInfo.nonEmpty should be (true)
-    response.latestReceiptInfo.map { latest =>
-      latest.purchaseDate should equal(latestPurchaseDate)
-    }
+//    val receipt = ReceiptInfo(
+//        originalPurchaseDate,
+//        "orig_trans_1233",
+//        "trans_1234",
+//        purchaseDate,
+//        "product_123"
+//    )
+//
+//    val json = ReceiptRenderer(ReceiptResponse(receipt))
+//    val response = AppleApi.parseResponse(json)
+//
+//    response.receipt.purchaseDate should equal (purchaseDate)
+//    response.receipt.originalPurchaseDate should equal (originalPurchaseDate)
+    fail("Needs to be reimplemented.")
   }
 }
